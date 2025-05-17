828b4b05b9db622a8c46bf1e7d475d6f16ee004d
wandalen <wandalen.me@gmail.com>
2025-05-17 18:31:42 +0300

    Merge content of wandalen/wVerbal into wVerbal

---
+++ wVerbal/proto/wtools/amid/amixin.test/Verbal.test.s
@@ -0,0 +1,54 @@
+( function _Verbal_test_s_( )
+{
+
+'use strict';
+
+/*
+qqq : implement tests for mixin vebal
+*/
+
+if( typeof module !== 'undefined' )
+{
+
+  const _ = require( '../../../node_modules/Tools' );
+
+  _.include( 'wTesting' );
+
+  require( '../amixin/Verbal.s' );
+
+}
+
+const _ = _global_.wTools;
+
+//
+
+function trivial( test )
+{
+
+  test.identical( 1, 1 );
+
+}
+
+//
+
+const Proto =
+{
+
+  name : 'Tools.mid.mixin.Verbal',
+  silencing : 1,
+  // verbosity : 1,
+
+  tests :
+  {
+    trivial,
+  },
+
+}
+
+//
+
+const Self = wTestSuite( Proto );
+if( typeof module !== 'undefined' && !module.parent )
+wTester.test( Self.name );
+
+} )( );
---
+++ wVerbal/sample/trivial/Sample.s
@@ -0,0 +1,10 @@
+
+if( typeof module !== 'undefined' )
+require( 'wverbal' );
+let _ = wTools;
+
+/**/
+
+console.log( _.Verbal );
+
+/* todo : implement */
