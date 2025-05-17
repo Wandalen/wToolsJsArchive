cd4f45da116f514160d5faf4ad8fa36480546b46
wandalen <wandalen.me@gmail.com>
2025-05-17 18:32:45 +0300

    Merge content of wandalen/wNjsCppBasic into wNjsCppBasic

---
+++ wNjsCppBasic/sample/trivial/Sample.s
@@ -0,0 +1,18 @@
+
+// let _ = require( 'wresolverextra' );
+// var src =
+// {
+//   dir :
+//   {
+//     val1 : 'Hello'
+//   },
+// }
+
+// var resolved = _.resolver.resolve( src, 'dir/val1' )
+// console.log( resolved );
+
+console.log( 'not implemented' );
+
+/*
+log : `Hello`
+*/
